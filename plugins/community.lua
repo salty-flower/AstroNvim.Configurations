e717d9c1cb3bf9241ddc8a83b48398e23d341783
salty-flower <95509524+salty-flower@users.noreply.github.com>
2023-08-26 16:08:15 +0800

    feat: language plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,8 +4,14 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.colorscheme.neosolarized-nvim" },
   { import = "astrocommunity.pack.ps1" },
+  { import = "astrocommunity.pack.cs" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.kotlin" },
+  { import = "astrocommunity.pack.scala" },
+  { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.rust" },
 }
