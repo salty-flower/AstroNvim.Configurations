6e8f3c588871ea798c168e997e9ac1589cb53842
salty-flower <95509524+salty-flower@users.noreply.github.com>
2023-08-26 16:06:21 +0800

    feat: use `user.lua`

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,6 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+  {
+    "kaarmu/typst.vim",
+    ft = { "typst" },
+  },
 }
