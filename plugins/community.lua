b53a1677ca8cf80f26b6859fa87a649ed7c7797c
salty-flower <95509524+salty-flower@users.noreply.github.com>
2023-08-26 16:07:02 +0800

    feat: neosolarized

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,6 +5,7 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.neosolarized-nvim" },
   { import = "astrocommunity.pack.ps1" },
   { import = "astrocommunity.pack.rust" },
 }
--- plugins/user.lua
+++ plugins/user.lua
@@ -3,4 +3,9 @@ return {
     "kaarmu/typst.vim",
     ft = { "typst" },
   },
+  { "tjdevries/colorbuddy.nvim" },
+  {
+    "rcarriga/nvim-notify",
+    opts = function(_, opts) opts.background_colour = "#000000" end,
+  },
 }
