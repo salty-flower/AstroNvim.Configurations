4704079b37025e40a7011fa2fab4400c03ed95e4
salty-flower <95509524+salty-flower@users.noreply.github.com>
2023-11-13 19:05:26 +0800

    feat: various plugins

--- plugins/user.lua
+++ plugins/user.lua
@@ -2,10 +2,27 @@ return {
   {
     "kaarmu/typst.vim",
     ft = { "typst" },
+    -- lazy = false,
   },
   { "tjdevries/colorbuddy.nvim" },
   {
     "rcarriga/nvim-notify",
     opts = function(_, opts) opts.background_colour = "#000000" end,
+  },
+  { "NoahTheDuke/vim-just", ft = { "just" } },
+  { "IndianBoy42/tree-sitter-just" },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+        },
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-dap",
+    enabled = true,
   },
 }
