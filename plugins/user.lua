6b0943004341e14435ccddbbef5e325648ddb3ed
salty-flower <95509524+salty-flower@users.noreply.github.com>
2023-11-19 16:35:52 +0800

    fix: make JoosepAlviste/nvim-ts-context-commentstring happy

--- plugins/user.lua
+++ plugins/user.lua
@@ -25,4 +25,7 @@ return {
     "mfussenegger/nvim-dap",
     enabled = true,
   },
+  -- due to JoosepAlviste/nvim-ts-context-commentstring breaking changes at 5b02387b28a79c61b7d406c2a33d4db1d8454f53
+  { "JoosepAlviste/nvim-ts-context-commentstring", opts = function(_, opts) opts.enable_autocmd = false end },
+  { "nvim-treesitter/nvim-treesitter", opts = function(_, opts) opts.context_commentstring = nil end },
 }
