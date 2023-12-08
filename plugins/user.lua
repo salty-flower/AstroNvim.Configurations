e458507956fb169fab1aeb9cdc580478896aacdf
salty-flower <95509524+salty-flower@users.noreply.github.com>
2023-12-08 18:59:30 +0800

    fix: make `nvim-treesitter` believe in Internet

--- plugins/user.lua
+++ plugins/user.lua
@@ -27,5 +27,11 @@ return {
   },
   -- due to JoosepAlviste/nvim-ts-context-commentstring breaking changes at 5b02387b28a79c61b7d406c2a33d4db1d8454f53
   { "JoosepAlviste/nvim-ts-context-commentstring", opts = function(_, opts) opts.enable_autocmd = false end },
-  { "nvim-treesitter/nvim-treesitter", opts = function(_, opts) opts.context_commentstring = nil end },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.context_commentstring = nil
+      opts.prefer_git = true
+    end,
+  },
 }
