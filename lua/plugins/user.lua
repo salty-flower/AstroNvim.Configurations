e045bd552f265d36eb377cdbf540fce1fad88394
salty-flower <95509524+salty-flower@users.noreply.github.com>
2025-02-24 10:30:54 +0800

    chore: migrate old plugin folder to newer format

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -88,6 +88,10 @@ return {
       },
     },
   },
+
+  { "max397574/better-escape.nvim" },
+
+  -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
   {
     "mfussenegger/nvim-dap",
     enabled = true,
@@ -126,6 +130,53 @@ return {
           },
         },
       }
+    end,
+  },
+  {
+    "windwp/nvim-autopairs",
+    config = function(plugin, opts)
+      require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
+      -- add more custom autopairs configuration such as custom rules
+      local npairs = require "nvim-autopairs"
+      local Rule = require "nvim-autopairs.rule"
+      local cond = require "nvim-autopairs.conds"
+      npairs.add_rules {
+        Rule("$", "$", { "tex", "latex" })
+          -- don't add a pair if the next character is %
+          :with_pair(cond.not_after_regex "%%")
+          -- don't add a pair if  the previous character is xxx
+          :with_pair(cond.not_before_regex("xxx", 3))
+          -- don't move right when repeat character
+          :with_move(cond.none())
+          -- don't delete if the next character is xx
+          :with_del(cond.not_after_regex "xx")
+          -- disable adding a newline when you press <cr>
+          :with_cr(cond.none()),
+      }
+    end,
+  },
+  { "tjdevries/colorbuddy.nvim" },
+  { "NoahTheDuke/vim-just", ft = { "just" } },
+  { "IndianBoy42/tree-sitter-just" },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+        },
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-dap",
+    enabled = true,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.context_commentstring = nil
+      opts.prefer_git = true
     end,
   },
   {
