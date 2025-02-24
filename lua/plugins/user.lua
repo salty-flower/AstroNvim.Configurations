6d9ed4da7eefba2b8b3834d6402b894f1cb2b639
salty-flower <95509524+salty-flower@users.noreply.github.com>
2025-02-24 10:30:54 +0800

    chore: migrate old plugin folder to newer format

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,19 +1,8 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- You can also add or configure plugins by creating files in this `plugins/` folder
--- PLEASE REMOVE THE EXAMPLES YOU HAVE NO INTEREST IN BEFORE ENABLING THIS FILE
--- Here are some examples:
-
 ---@type LazySpec
 return {
-
-  -- == Examples of Adding Plugins ==
-
-  "andweeb/presence.nvim",
   {
-    "ray-x/lsp_signature.nvim",
-    event = "BufRead",
-    config = function() require("lsp_signature").setup() end,
+    "kaarmu/typst.vim",
+    ft = { "typst" },
   },
 
   -- == Examples of Overriding Plugins ==
@@ -55,7 +44,6 @@ return {
       luasnip.filetype_extend("javascript", { "javascriptreact" })
     end,
   },
-
   {
     "windwp/nvim-autopairs",
     config = function(plugin, opts)
@@ -79,10 +67,38 @@ return {
             :with_del(cond.not_after_regex "xx")
             -- disable adding a newline when you press <cr>
             :with_cr(cond.none()),
-        },
-        -- disable for .vim files, but it work for another filetypes
-        Rule("a", "a", "-vim")
+        }
       )
+    end,
+  },
+  { "tjdevries/colorbuddy.nvim" },
+  -- {
+  --   "rcarriga/nvim-notify",
+  --   opts = function(_, opts) opts.background_colour = "#000000" end,
+  -- },
+  { "NoahTheDuke/vim-just", ft = { "just" } },
+  { "IndianBoy42/tree-sitter-just" },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+        },
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-dap",
+    enabled = true,
+  },
+  -- due to JoosepAlviste/nvim-ts-context-commentstring breaking changes at 5b02387b28a79c61b7d406c2a33d4db1d8454f53
+  { "JoosepAlviste/nvim-ts-context-commentstring", opts = function(_, opts) opts.enable_autocmd = false end },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.context_commentstring = nil
+      opts.prefer_git = true
     end,
   },
 }
