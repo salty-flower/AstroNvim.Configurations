d0044c33cad51a3e9b0e197cc18924d2df64d0eb
salty-flower <95509524+salty-flower@users.noreply.github.com>
2025-02-25 11:41:23 +0800

    feat: add leetcode

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -101,4 +101,18 @@ return {
       opts.prefer_git = true
     end,
   },
+  {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html", -- if you have `nvim-treesitter` installed
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      -- "ibhagwan/fzf-lua",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+    },
+    opts = {
+      -- configuration goes here
+      lang = "python3",
+    },
+  },
 }
