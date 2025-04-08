b718353b4306b6bbc00838347abb97d79302949b
salty-flower <95509524+salty-flower@users.noreply.github.com>
2025-04-08 16:07:19 +0800

    feat: improve leetcode experience - fast-enter and auto-imports

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,4 +1,4 @@
 return {
   "goolord/alpha-nvim",
-  opts = function(_, opts) table.insert(opts.section.buttons.val, opts.button("lt", "  LeetCode", ":Leet")) end,
+  opts = function(_, opts) table.insert(opts.section.buttons.val, opts.button("lt", "  LeetCode", ":Leet<ENTER>")) end,
 }
