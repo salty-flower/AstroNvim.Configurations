9e7d0fbdffac38ff0d6062b56c414d4892c50d16
salty-flower <95509524+salty-flower@users.noreply.github.com>
2025-08-25 20:13:53 +0800

    style: format

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -52,23 +52,19 @@ return {
       local npairs = require "nvim-autopairs"
       local Rule = require "nvim-autopairs.rule"
       local cond = require "nvim-autopairs.conds"
-      npairs.add_rules(
-        {
-          Rule("$", "$", { "tex", "latex" })
-            -- don't add a pair if the next character is %
-            :with_pair(cond.not_after_regex "%%")
-            -- don't add a pair if  the previous character is xxx
-            :with_pair(
-              cond.not_before_regex("xxx", 3)
-            )
-            -- don't move right when repeat character
-            :with_move(cond.none())
-            -- don't delete if the next character is xx
-            :with_del(cond.not_after_regex "xx")
-            -- disable adding a newline when you press <cr>
-            :with_cr(cond.none()),
-        }
-      )
+      npairs.add_rules {
+        Rule("$", "$", { "tex", "latex" })
+          -- don't add a pair if the next character is %
+          :with_pair(cond.not_after_regex "%%")
+          -- don't add a pair if  the previous character is xxx
+          :with_pair(cond.not_before_regex("xxx", 3))
+          -- don't move right when repeat character
+          :with_move(cond.none())
+          -- don't delete if the next character is xx
+          :with_del(cond.not_after_regex "xx")
+          -- disable adding a newline when you press <cr>
+          :with_cr(cond.none()),
+      }
     end,
   },
   { "tjdevries/colorbuddy.nvim" },
@@ -209,5 +205,5 @@ return {
       auto_open = false,
     },
   },
-  {"kevinhwang91/nvim-ufo", enabled=false}
+  { "kevinhwang91/nvim-ufo", enabled = false },
 }
