7d7a5999142d2dceb444380629471db1b2dc9950
salty-flower <95509524+salty-flower@users.noreply.github.com>
2026-01-03 16:40:47 +0800

    chore: remove some duplicate

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -44,6 +44,7 @@ return {
       luasnip.filetype_extend("javascript", { "javascriptreact" })
     end,
   },
+
   {
     "windwp/nvim-autopairs",
     config = function(plugin, opts)
@@ -67,33 +68,18 @@ return {
       }
     end,
   },
+
   { "tjdevries/colorbuddy.nvim" },
+
   {
     "rcarriga/nvim-notify",
     opts = function(_, opts) opts.background_colour = "#000000" end,
   },
+
   { "NoahTheDuke/vim-just", ft = { "just" } },
-  { "IndianBoy42/tree-sitter-just" },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-      filesystem = {
-        filtered_items = {
-          visible = true,
-        },
-      },
-    },
-  },
 
-  { "max397574/better-escape.nvim" },
-
-  -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
-  {
-    "mfussenegger/nvim-dap",
-    enabled = true,
-  },
-  -- due to JoosepAlviste/nvim-ts-context-commentstring breaking changes at 5b02387b28a79c61b7d406c2a33d4db1d8454f53
   { "JoosepAlviste/nvim-ts-context-commentstring", opts = function(_, opts) opts.enable_autocmd = false end },
+
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
@@ -101,9 +87,10 @@ return {
       opts.prefer_git = true
     end,
   },
+
   {
     "kawre/leetcode.nvim",
-    build = ":TSUpdate html", -- if you have `nvim-treesitter` installed
+    build = ":TSUpdate html",
     dependencies = {
       "ibhagwan/fzf-lua",
       "nvim-lua/plenary.nvim",
@@ -130,36 +117,7 @@ return {
       },
     },
   },
-  {
-    "windwp/nvim-autopairs",
-    config = function(plugin, opts)
-      require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
-      -- add more custom autopairs configuration such as custom rules
-      local npairs = require "nvim-autopairs"
-      local Rule = require "nvim-autopairs.rule"
-      local cond = require "nvim-autopairs.conds"
-      npairs.add_rules {
-        Rule("$", "$", { "tex", "latex" })
-          -- don't add a pair if the next character is %
-          :with_pair(cond.not_after_regex "%%")
-          -- don't add a pair if  the previous character is xxx
-          :with_pair(cond.not_before_regex("xxx", 3))
-          -- don't move right when repeat character
-          :with_move(cond.none())
-          -- don't delete if the next character is xx
-          :with_del(cond.not_after_regex "xx")
-          -- disable adding a newline when you press <cr>
-          :with_cr(cond.none()),
-      }
-    end,
-  },
-  { "tjdevries/colorbuddy.nvim" },
-  {
-    "rcarriga/nvim-notify",
-    opts = function(_, opts) opts.background_colour = "#000000" end,
-  },
-  { "NoahTheDuke/vim-just", ft = { "just" } },
-  { "IndianBoy42/tree-sitter-just" },
+
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
@@ -170,40 +128,26 @@ return {
       },
     },
   },
-  {
-    "mfussenegger/nvim-dap",
-    enabled = true,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      opts.context_commentstring = nil
-      opts.prefer_git = true
-    end,
-  },
-  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "rcarriga/nvim-notify",
-    },
-  },
+
+  { "mfussenegger/nvim-dap", enabled = true },
+
   {
     "lervag/vimtex",
-    -- SumatraPDF is used by default on Windows, so no config for viewer is needed
   },
+
   {
     "nvim-treesitter/nvim-treesitter-context",
     opts = {
       enable = true,
     },
   },
+
   {
     "folke/trouble.nvim",
     opts = {
       auto_open = false,
     },
   },
+
   { "kevinhwang91/nvim-ufo", enabled = false },
 }
--- lua/polish.lua
+++ lua/polish.lua
@@ -1,5 +1,6 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+
+-- Performance optimization: skip deprecated treesitter context commentstring module
+vim.g.skip_ts_context_commentstring_module = true
